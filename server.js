47449bc74807fe02de6ba3028e7bfcfaeb71f9f0
joseluisgalvanramirez <joseluisgalvanramirez@outlook.com>
2026-04-23 11:19:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5501;
+const PORT = process.env.PORT || 5500;
 
 // Middlewares
 app.use(cors());
